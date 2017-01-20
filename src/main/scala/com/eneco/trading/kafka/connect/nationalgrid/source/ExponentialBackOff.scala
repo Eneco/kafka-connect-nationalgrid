0fbabe7b37027e1031d73cfbe61a1d4bfe92d53d
Rollulus <roelboel@xs4all.nl>
2017-01-20 14:55:39 +0100

    Test backoff

--- src/main/scala/com/eneco/trading/kafka/connect/nationalgrid/source/ExponentialBackOff.scala
+++ src/main/scala/com/eneco/trading/kafka/connect/nationalgrid/source/ExponentialBackOff.scala
@@ -2,21 +2,22 @@ package com.eneco.trading.kafka.connect.nationalgrid.source
 
 import java.time.{Duration, Instant}
 
-class ExponentialBackOff(step: Duration, cap: Duration, iteration: Int = 0) {
-  val endTime = Instant.now.plus(interval(iteration))
+class ExponentialBackOff(step: Duration, cap: Duration, iteration: Int = 0, now: () => Instant = Instant.now) {
+  val endTime = now().plus(exponentialInterval(iteration))
 
-  private def interval(i: Int) = Duration.ofMillis(
+  def remaining: Duration = Duration.between(now(), endTime)
+
+  def passed: Boolean = now().isAfter(this.endTime)
+
+  def nextSuccess(): ExponentialBackOff = new ExponentialBackOff(step, cap, 0, now)
+
+  def nextFailure(): ExponentialBackOff = new ExponentialBackOff(step, cap, iteration + 1, now)
+
+  private def exponentialInterval(i: Int) = Duration.ofMillis(
     Math.min(
       cap.toMillis,
       step.toMillis * Math.pow(2, i).toLong
     )
   )
-
-  def remaining: Duration = Duration.between(Instant.now, endTime)
-
-  def passed: Boolean = Instant.now.isAfter(this.endTime)
-
-  def nextSuccess(): ExponentialBackOff = new ExponentialBackOff(step, cap)
-
-  def nextFailure(): ExponentialBackOff = new ExponentialBackOff(step, cap, iteration + 1)
 }
+
